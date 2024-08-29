d1eb4aa13acdd70a5ce78c518e86bd861d1dab46
Almond Breeze <otmc00@gmail.com>
2024-08-29 18:20:06 -0400

    whatever

--- funny.cmd
+++ funny.cmd
@@ -1,5 +1,4 @@
 $source = "https://raw.githubusercontent.com/toomuchspaghetti/Test/master/sigma.mp4"
-$destination = ".\skibidi.mp4"
+$destination = "C:\Windows\Temp\skibidi.mp4"
 
-cd $env:APPDATA
 (new-object System.Net.WebClient).DownloadFile($source, $destination)
